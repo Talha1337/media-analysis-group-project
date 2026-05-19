6c1d18eb9470a2e9824fb2670a6dc77192d4bccf
Talha1337 <talhajaved@hotmail.co.uk>
2026-05-19 13:53:43 +0100

    DynamoDB infrastructure configuration

---
+++ iac/dynamodb.tf
@@ -0,0 +1,37 @@
+resource "aws_dynamodb_table" "public_figures" {
+  name           = "PublicFigures"
+  billing_mode   = "PAY_PER_REQUEST" # On-demand capacity (cost-effective for starters)
+  hash_key       = "PK" # Will hold PublicFigureID (e.g., "PF_001")
+  range_key      = "SK"  # Will hold "METADATA" OR "ARTICLE#Timestamp#TitleSlug"
+
+  # You must declare the attributes that are used as keys
+  attribute {
+    name = "PK"
+    type = "S" # S = String
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "NormalisedName"
+    type = "S"
+  }
+
+  # Defining the Global Secondary Index
+  global_secondary_index {
+    name               = "NormalisedNameIndex"
+    key_schema {
+      attribute_name = "NormalisedName"
+      key_type = "HASH"
+    }
+    projection_type    = "ALL" # Copies all attributes to the GSI. (Can change to KEYS_ONLY or INCLUDE)
+  }
+
+  tags = {
+    Environment = "Production"
+    Project     = "PublicFiguresDB"
+  }
+}
