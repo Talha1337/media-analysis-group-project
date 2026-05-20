7d9dd62c1e60155785d78699eb97eaf20c0e9202
carolaguev <grcarol@icloud.com>
2026-05-20 18:34:25 +0100

    Issue #45: Adds load.py function definitions

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,1 +1,27 @@
 """A load script to interact with DynamoDB on AWS."""
+
+from boto3 import client
+from botocore.exceptions import ClientError
+
+
+def connect_to_dynamodb() -> client:
+    """Initialize connection to DynamoDB table."""
+
+
+def generate_article_sort_key(timestamp: str, title: str) -> str:
+    """Creates a unique article SK with the format: 
+    ARTICLE#[FeedName]#[ISO8601Timestamp]#[TitleHash]"""
+
+
+def assign_feed_id(feed_name: str) -> str:
+    """Assigns a unique feed ID based on the feed name."""
+
+
+def prepare_item_for_load(article: dict, public_figure_name: str) -> dict:
+    """Converts enriched data for each article to DynamoDB item format with: 
+    PK, SK, feed_id, names, published_at, sentiment, keywords."""
+
+
+def load_all_items(enriched_articles: list[dict]) -> None:
+    """Batch loads items into DynamoDB, partitioned by public figure."""
+    
