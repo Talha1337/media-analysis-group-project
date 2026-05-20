7fcf2f003b954d22581e01fffa4951490d5a4982
Talha1337 <98235118+Talha1337@users.noreply.github.com>
2026-05-20 12:46:47 +0100

    Delete dashboard/deploy_image.sh

--- dashboard/deploy_image.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-docker build -t c23-epipelagic-ecr-dynamo  --provenance=false --platform="linux/amd64" .
-docker tag c23-epipelagic-ecr-dynamo:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c23-epipelagic-ecr-dynamo:latest
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c23-epipelagic-ecr-dynamo:latest
