4a892aa1c01873509eb354c4d3a7de07839366ca
Talha1337 <talhajaved@hotmail.co.uk>
2026-05-20 16:06:20 +0100

    alter tf configurations for removed ecs task and lambda permission

--- iac/ecstask.tf
+++ iac/ecstask.tf
@@ -7,73 +7,15 @@ resource "aws_ecr_repository" "c23_epipelagic_ecr_dynamo" {
   }
 }
 
-data "aws_iam_role" "ecs_task_execution_role" {
-    name = "ecsTaskExecutionRole"
-}
-
-data "aws_iam_policy_document" "c23_epipelagic_task_role_doc" {
-  # First block: Athena
-statement {
-    sid    = "DynamoDBReadAccess"
-    effect = "Allow"
-    actions = [
-      "dynamodb:GetItem",
-      "dynamodb:BatchGetItem",
-      "dynamodb:Scan",
-      "dynamodb:Query",
-      "dynamodb:ConditionCheckItem"
-    ]
-    resources = ["${aws_dynamodb_table.c23_epipelagic_public_figures.arn}"]
+resource "null_resource" "dummy_image" {
+  provisioner "local-exec" {
+    command = <<EOF
+      aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${aws_ecr_repository.c23_epipelagic_ecr_dynamo.repository_url}
+      docker pull public.ecr.aws/lambda/provided:al2023
+      docker tag public.ecr.aws/lambda/provided:al2023 ${aws_ecr_repository.c23_epipelagic_ecr_dynamo.repository_url}:latest
+      docker push ${aws_ecr_repository.c23_epipelagic_ecr_dynamo.repository_url}:latest
+    EOF
   }
-}
 
-# Then you turn that data into an actual policy resource
-resource "aws_iam_policy" "c23_epipelagic_task_role_policy" {
-  name   = "c23-epipelagic-task-role-policy"
-  policy = data.aws_iam_policy_document.c23_epipelagic_task_role_doc.json
-}
-
-resource "aws_iam_role" "c23_epipelagic_task_role" {
-  name = "c23-epipelagic-task-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "task_role_attachment" {
-  role       = aws_iam_role.c23_epipelagic_task_role.name
-  policy_arn = aws_iam_policy.c23_epipelagic_task_role_policy.arn
-}
-
-resource "aws_ecs_task_definition" "c23_epipelagic_ecs_task" {
-  family = "c23-epipelagic-ecs-task"
-  requires_compatibilities = ["FARGATE"]
-  cpu = 1024
-  memory = 2048
-
-  network_mode = "awsvpc"
-  task_role_arn = "${aws_iam_role.c23_epipelagic_task_role.arn}"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
-  container_definitions = jsonencode([
-    {
-      name      = "c23-epipelagic-dynamo-container"
-      image     = "${aws_ecr_repository.c23_epipelagic_ecr_dynamo.repository_url}:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8000
-          hostPort      = 8000
-        }
-      ]
-    }
-  ])
+  depends_on = [aws_ecr_repository.c23_epipelagic_ecr_dynamo]
 }
