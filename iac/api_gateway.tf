bf44a9230cf9e7cbda35406f16728a8f232a2195
Talha1337 <talhajaved@hotmail.co.uk>
2026-05-20 10:14:07 +0100

    add keywords function signature

---
+++ iac/api_gateway.tf
@@ -0,0 +1,11 @@
+resource "aws_apigatewayv2_api" "c23_epipelagic_http_api" {
+  name          = "c23-epipelagic-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "c23_epipelagic_lambda_integration" {
+  api_id             = aws_apigatewayv2_api.c23_epipelagic_http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.c23_epipelagic_lambda_function.invoke_arn
+  integration_method = "POST"
+}
