17b199c2d4e9d3a801bc1385a75921647a7f2896
carolaguev <grcarol@icloud.com>
2026-05-21 19:20:45 +0100

    Changes to tests for load phase

--- pipeline/load.py
+++ pipeline/load.py
@@ -9,20 +9,24 @@ def connect_to_dynamodb() -> BaseClient:
     """Initialise connection to DynamoDB."""
 
 
-def assign_feed_id(feed_name: str) -> str:
-    """Assigns a unique feed ID based on the feed name."""
+def assign_feed_id(feed_link: str) -> str:
+    """Assigns a unique feed ID based on the feed link."""
 
 
-def generate_article_sort_key(published_at: str, feed_id: str, article_id: str) -> str:
+def assign_article_id(article_link: str) -> str:
+    """Assigns a unique article ID based on the article link."""
+
+
+def generate_article_sk(published_at: str, feed_id: str, article_id: str) -> str:
     """Creates a unique article SK with the format: 
     ARTICLE#[feed_id]#[published_at]#[article_id]"""
 
 
-def prepare_item_for_load(enriched_article: dict, name: str) -> dict:
+def prepare_item_for_load(article: dict, name: str) -> dict:
     """Converts enriched data for each article to DynamoDB item format with: 
     PK (name), SK, feed_id, names, published_at, sentiment_score, key_words."""
 
 
-def load_all_items(enriched_articles: list[dict]) -> None:
+def load_all_items(articles: list[dict]) -> None:
     """Batch loads items into DynamoDB, partitioned by each identified name."""
     
